e21809f6d362fb00cb1f4777b2797360f77dc27e
leitingting08 <leitingting08@gmail.com>
2017-12-14 15:04:18 +0800

    initial

--- src/page/common/nav-simple/index.js
+++
@@ -1,8 +0,0 @@
-/*
-* @Author: Administrator
-* @Date:   2017-11-11 11:26:01
-* @Last Modified by:   Administrator
-* @Last Modified time: 2017-11-11 11:26:33
-*/
-
-require('./index.css');
--- src/page/module.js
+++
@@ -1,9 +0,0 @@
-/*
-* @Author: Administrator
-* @Date:   2017-11-05 16:03:57
-* @Last Modified by:   Administrator
-* @Last Modified time: 2017-11-05 16:04:34
-*/
-module.exports = {
-    test: 1234
-}
