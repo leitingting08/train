d6f01c8562eaf3b5af785772f5b74e3525f01c89
leitingting08 <leitingting08@gmail.com>
2017-12-17 19:49:45 +0800

    trainList

--- src/page/train-list.vue
+++ src/page/train-list.vue
@@ -91,8 +91,8 @@ export default{
 .orange{color: @orange;}
 .train-list{
     .v-title{position: fixed;top: 0;width: 100%;z-index: 100;}
-    .time-calender{position: fixed;top: 0.9rem;width: 100%;z-index: 100;}
-  .lists{position: absolute;top: 1.7rem;width: 100%;
+    .time-calender{position: fixed;top: 0.8rem;width: 100%;z-index: 100;}
+  .lists{position: absolute;top: 1.6rem;width: 100%;
   .list{background: #fff;margin:0.1rem 0;
     .vux-flexbox{padding:0.2rem 0;
      .l{padding-left: 0.2rem}
