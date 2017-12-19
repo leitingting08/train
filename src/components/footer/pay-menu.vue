4bb611e8e4dcd3450891153a4d3f7c969953bd5e
leitingting08 <leitingting08@gmail.com>
2017-12-19 09:16:01 +0800

    trainOrderFill

--- src/components/footer/pay-menu.vue
+++ src/components/footer/pay-menu.vue
@@ -2,7 +2,7 @@
     <div class="pay-menu">
       <flexbox>
         <flexbox-item class="price-detail"><div class="flex-demo l">
-            <div class="orange font30"><span class="font20">￥</span>92.5<span class="col999 font20">（共1人）</span><span></span></div>
+            <div class="orange font30 price"><span class="font20">￥</span>92.5<span class="col999 font20">（共1人）</span><span></span></div>
             <div>订单总价</div>
             <span class="detail">明细<i class="fa fa-angle-down"></i></span>
             </div>
@@ -33,11 +33,13 @@ export default {
 .font30{font-size: 0.3rem;}
 .pay-menu{background: #fff;position: fixed;bottom: 0;width: 100%;
     .pay{background-color: @orange;}
-   .flex-demo{text-align: center;position: relative;
-    // i{position: absolute;bottom: 0.35rem;left: 45%}
-    .l{position: relative;
-        .detail{position: absolute;top:0.2rem;}
-    }
+   .flex-demo{text-align: center;position: relative;height: 1rem;
   }
+  .l{position: relative;text-align: left;margin-left: 0.2rem;
+        .detail{position: absolute;top:0.2rem;right: 0.2rem;}
+        div{height: 0.4rem;}
+        .price{padding-top: 0.1rem;}
+    }
+    .r{line-height: 1rem;color: #fff;font-size: 0.32rem;}
 }
 </style>
