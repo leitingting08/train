f3d99b5f0e9ee6440832b9aed32f5a070786d3a9
leitingting08 <leitingting08@gmail.com>
2017-12-21 13:38:10 +0800

    citylist

--- src/components/cityselect/train-city.vue
+++ src/components/cityselect/train-city.vue
@@ -13,6 +13,22 @@
 					<div class="search-wrapper">
 						 <input type="text"  placeholder="中文/拼音/首字母" @keyup="autoInput()"/>
 					</div>
+					<!-- <div class="auto-list-con">
+						<div class="list-name" v-for="item in autoData">{{item.stationName}}</div>
+					</div> -->
+					<div class="list-con">
+						<div class="tit">最近访问</div>
+			            <div class="list-other">
+			                <span class="tag-name" v-for="(city,index) in citys">{{city.searchcitys}}</span>
+			            </div>
+			            <div class="tit">热门车站</div>
+			            <div class="list-other">
+			                <span class="tag-name" v-for="(item,index) in hotcitys">{{item}}</span>
+			            </div>
+                        <div class="list-name" v-for="(item,index) in listData">
+                              <div class="name" v-for="city in Stations">{{city.sta_name}}</div>
+                        </div>
+					</div>
 				</div>
 			</div>
 		</popup>
@@ -21,6 +37,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 import {TransferDom,Popup,InlineLoading } from 'vux';
 
 export default{
@@ -29,17 +46,36 @@ export default{
 		TransferDom
 	},
 	components:{TransferDom,Popup,InlineLoading},
+	created(){
+       this.loadCityList();
+      
+	},
 	// props:['cityname'],
 	data(){
 		return{
 			// name:this.cityName
 			changeCityName:'杭州',
-			cityIsShow:false
+			cityIsShow:false,
+			citys:[{'searchcitys':'杭州'}],
+			hotcitys:['杭州','北京'],
+			listData:{},
+			Stations: []
 		}
 	},
 	methods:{
 		showCityList(){
 			this.cityIsShow=true;
+		},
+		loadCityList(){
+			axios.get('src/api/station-list.json')
+			.then((res)=>{
+				let Stations=[];
+				Stations.push(res.data.result);
+                Stations=Stations[0]
+			})
+			.catch((err)=>{
+				console.log(err);
+			})
 		}
 	}
 	// computed:{
@@ -61,6 +97,14 @@ export default{
     .city{padding: 0.2rem;
          .search-wrapper{
          	input{width: 97%;height: 0.5rem;border:1px solid #999;padding-left: 0.2rem;border-radius: 3px;}
+         }
+         .list-con{padding-top: 0.3rem;
+         	.list-other{
+         		.tag-name{display:inline-block;padding: 0.15rem 0.3rem;border:1px solid #d8d8d8;background: #fff;border-radius: 5px;margin:0.2rem 0.1rem;}
+         		.list-name{
+         			.name{display: inline-block;width: 100%;height: 0.6rem;}
+         		}
+         	}
          }
     }
 }
