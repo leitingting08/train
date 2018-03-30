b199074c2614e8e99dae08176170079377b4ae0a
leitingting08 <leitingting08@gmail.com>
2018-03-30 18:27:23 +0800

    server

--- config/index.js
+++ config/index.js
@@ -10,11 +10,7 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {
-        '/stations':{
-            target:'http://localhost:3000'
-        }
-    },
+   
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
