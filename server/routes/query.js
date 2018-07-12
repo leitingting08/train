7475f8e707c47f17ab1258d41646e701d45b8ba6
leitingting08 <leitingting08@gmail.com>
2018-07-12 18:14:57 +0800

    query

--- server/routes/query.js
+++ server/routes/query.js
@@ -1,80 +1,35 @@
-/*
-* @Author: Administrator
-* @Date:   2018-04-18 19:10:27
-* @Last Modified by:   Administrator
-* @Last Modified time: 2018-04-18 20:16:54
-*/
-// var express = require('express');
-// var router = express.Router();
-// // var mongoose = require('mongoose');
-// var Query = require('../models/query');
+const express = require('express');
+const router = express.Router();
+const urllib = require('url');
+const request = require('request');
 
+router.get("/query", function(req,res,next){
+    const obj = req.body;
+    const url = '/otn/leftTicket/query?leftTicketDTO.train_date=2018-08-07&leftTicketDTO.from_station=HZH&leftTicketDTO.to_station=MCN&purpose_codes=ADULT';
+    const options={
+        hostnme:'kyfw.12306.cn',
+        port:443,
+        path:url,
+        method:'GET',
+        rejectUnauthorized:false
+    };
+    console.log(options)
+    // let request=https.request(options,function(response){
+    //     response.setEncoding('utf8');
 
+    //     const str='';
 
-// router.get("/", function(req,res,next){
-//     var _callbcak = req.query.callbcak;
-//     var _data = {};
-//     if (_callbcak){
-//         res.type('text/javascript');
-//         res.send(_callbcak + '('+JSON.stringify(_data)+ ')');
-//     }
-//     else{
-//         res.json(_data)
-//     }
-//     // Query.find({}, function(err,doc){
-//     //     if(err){
-//     //         res.json({
-//     //             status:'1',
-//     //             msg:err.message
-//     //         });
+    //     response.on('data',function(chunk){
+    //         str+=chunk;
+    //     });
 
-//     //     }else{
-//     //         res.json({
-//     //             status:'0',
-//     //             msg:'',
-//     //             result:{
-//     //                 count:doc.length,
-//     //                 list:doc
-//     //             }
-//     //         })
-//     //     }
-//     // })
-// });
+    //     //完成拼装
+    //     request.on('error',funcion(e){
+    //         console.log('错误信息：'+e.message);
+    //     });
+    //     request.end();
+    // })
 
-// module.exports = router;
+});
 
-var request=require('request');
-var mongoose=require('mongoose');
-var moment=require('moment');
-
-
-
-exports.railway=function(req,res){
-    var obj=req.body;
-    var url='/otn/leftTicket/query?leftTicketDTO.train_date=20160831&leftTicketDTO.from_station=上海&leftTicketDTO.to_station=北京&purpose_codes=ADULT';
-var options={
-    hostnme:'kyfw.12306.cn',
-    port:443,
-    path:url,
-    method:'GET',
-    rejectUnauthorized:false
-
-};
-
-var request=https.request(options,function(response){
-    response.setEncoding('utf8');
-
-    var str='';
-
-    response.on('data',function(chunk){
-        str+=chunk;
-    });
-
-    //完成拼装
-    request.on('error',funcion(e){
-        console.log('错误信息：'+e.message);
-    });
-    request.end();
-})
-
-}
+module.exports = router;
