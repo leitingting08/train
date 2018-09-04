85454d153578f2c1e7d60bae6e6ea30752400ddb
leitingting08 <leitingting08@gmail.com>
2018-09-04 20:49:30 +0800

    query_trains

--- server/routes/demo.js
+++ server/routes/demo.js
@@ -4,72 +4,97 @@ var https = require('https');
 var http = require('http');
 var querystring = require('querystring');
 var fs = require('fs');
-// var ca = fs.readFileSync('../cert/srca.cer.pem');
+var ca = fs.readFileSync('../cert/srca.cer.pem');
 // http://192.168.1.248:3307/leave/defaultAudit
 
-var reqData = {currentPage:"1",
-				departmentName:"",
-				emId:"127",
-				entryEndTime:"",
-				entryStartTime:"",
-				items:[],
-				leaveName:"",
-				leaveType:"",
-				perRange:[],
-				roleName:"超级管理员",
-				state:""}
-        var postData = querystring.stringify(reqData);
-        var opts = {
-            method:'POST',
-            host:'192.168.1.248',
-            port:'3307',
-            path:'/leave/AllApply',
-            headers:{
-                'Content-Type':'application/x-www-form-urlencoded',
-                'Content-Length':postData.length
-            }
-        }
-
-        var config = {
-            time:'2018-09-17',//日期格式必须是这样
-            from_station:'BJP',//始发站车站代码，这里是北京北
-            end_station:'XMS',//车次
-        };
-        // const url = 'https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date='+config.time+'&leftTicketDTO.from_station='+config.from_station+'&leftTicketDTO.to_station='+config.end_station+'&purpose_codes=ADULT';
+// var reqData = {currentPage:"1",
+// 				departmentName:"",
+// 				emId:"127",
+// 				entryEndTime:"",
+// 				entryStartTime:"",
+// 				items:[],
+// 				leaveName:"",
+// 				leaveType:"",
+// 				perRange:[],
+// 				roleName:"超级管理员",
+// 				state:""}
+//         var postData = querystring.stringify(reqData);
+//         var opts = {
+//             method:'POST',
+//             host:'192.168.1.248',
+//             port:'3307',
+//             path:'/leave/AllApply',
+//             headers:{
+//                 'Content-Type':'application/x-www-form-urlencoded',
+//                 'Content-Length':postData.length
+//             }
+//         }
+// var reqData = {
+//     leftTicketDTO:{
+//         train_date: '2018-09-17',
+//         from_station: 'HZH',
+//         to_station: 'MCN'
+//     },
+//     purpose_codes: 'ADULT'
+// }
+// var reqData = {station_version:'1.9063'}
+// var reqData = {
+//     type: 'movie',
+//     tag: '热门',
+//     sort: 'recommend',
+//     page_limit: 20,
+//     page_start: 0
+// }
+var reqData = {
+    para: {"TimeStamp":1535957384.118,"MemberId":"","ConstId":"5b8cd96b06IXBeGb98TEUzsmyFUckXC1asLvdPI1","PlatId":432,"From":"杭州东","To":"麻城北","Date":"2018-09-17","OrderBy":0,"IsStudent":false,"TrainNo":""},
+    sign: 'ded9b698883875b33498935abad30b89'
+}
+// https://m.ctrip.com/restapi/soa2/10103/json/GetGrabTicketSucRateInfo?_fxpcqlniredt=09031094412093578760
+var postData = querystring.stringify(reqData);
         var options = { 
-            hostname: 'kyfw.12306.cn',//12306
-            path: '/otn/leftTicket/queryA?leftTicketDTO.train_date='+config.time+'&leftTicketDTO.from_station='+config.from_station+'&leftTicketDTO.to_station='+config.end_station+'&purpose_codes=ADULT',
-            // ca:[ca],//证书
-            rejectUnauthorized:false 
+            // hostname: 'kyfw.12306.cn',//12306
+            // hostname: 'movie.douban.com',//豆瓣
+            hostname: 'm.ly.com',//携程
+            path:'/uniontrain/webapi/trainsearch.html',
+            // path:'/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=20&page_start=0',
+            // path:'/otn/resources/js/framework/station_name.js?station_version=1.9063',
+            // path: '/otn/leftTicket/queryA?leftTicketDTO.train_date='
+            // +reqData.leftTicketDTO.train_date
+            // +'&leftTicketDTO.from_station='
+            // +reqData.leftTicketDTO.from_station+'&leftTicketDTO.to_station='
+            // +reqData.leftTicketDTO.to_station+'&purpose_codes=ADULT',
+            port: 443,
+            method:'POST',
+            rejectUnauthorized:false,
+            // key :ca, //证书
+            // cert :ca, //证书
+            agent: false,
+            headers:{
+                    "Content-Type": 'application/json',
+                    "Content-Length": postData.length
+            } 
          };
+// const url = 'https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=20&page_start=0'
+const url = 'https://m.ly.com/universal/touch/#/list?startdatetime=2018-09-17&startname=%E6%9D%AD%E5%B7%9E%E4%B8%9C&startcity=%E6%9D%AD%E5%B7%9E&arrivename=%E9%BA%BB%E5%9F%8E%E5%8C%97&arrivecity=%E9%BA%BB%E5%9F%8E&type=2055-9-0-1022-9-0&onlygd=0&student=0&origin=index'
+// const url = 'https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date=2018-09-17&leftTicketDTO.from_station=HZH&leftTicketDTO.to_station=MCN&purpose_codes=ADULT'
         //  const url = 'http://127.0.0.1:3000/stations'
         // var req = http.get(url,function(res){
-        var req = https.get(options,function(res){
+        const req = https.request(url,function(res){
         // var req = https.request(opts,function(res){
             // console.log(res);
-            var datas = [];
             var json = '';
-            var size = 0;
             res.on('data',function(data){
+                process.stdout.write(data);
+                console.log(process.stdout.write(data));
                 json += data
-                datas.push(data);
-                size += data.length;
-                console.log(json);// 这个写法是对的，可以获取到数据
             })
             res.on('end', function(chunk){
-                var buff = Buffer.concat(datas, size);
-                // console.log(chunk);
-                console.log('huoqu'+ json);
-                // console.log('获取的post数据为：' + chunk);
-                // console.log(datas);
-                // console.log(buff);
-                // callback({success:true, data:str});
+                console.log('获取的json是'+ json);
             })
         })
-        // console.log(req.agent.sockets);
    
         req.on('error',function(err){
             console.log('异常,异常原因'+err);
-            // callback({success:false, msg:'失败'});
         })
         // req.write(postData);options
+        req.end();
--- server/routes/query.js
+++ server/routes/query.js
@@ -7,116 +7,48 @@ var cheerio = require('cheerio')
 var express = require('express');
 var app = express();
 var router = express.Router();
+
+
 var config = {
     time:'2018-09-17',//日期格式必须是这样
     from_station:'BJP',//始发站车站代码，这里是北京北
     end_station:'XMS',//车次
 };
 
-const url = 'https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date='+config.time+'&leftTicketDTO.from_station='+config.from_station+'&leftTicketDTO.to_station='+config.end_station+'&purpose_codes=ADULT';
 
+const url = 'https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date='+config.time+'&leftTicketDTO.from_station='+config.from_station+'&leftTicketDTO.to_station='+config.end_station+'&purpose_codes=ADULT'
 
-// app.get('/', function (req, res, next) {
-    var options = { 
-        hostname: 'kyfw.12306.cn',//12306
-        path: '/otn/leftTicket/queryA?leftTicketDTO.train_date='+config.time+'&leftTicketDTO.from_station='+config.from_station+'&leftTicketDTO.to_station='+config.end_station+'&purpose_codes=ADULT',
-        // ca:[ca],//证书
-        // key: fs.readFileSync('../cert/server.key'),//这是我在ssl目录下生成的server.key改名为server.pem
-        // cert: fs.readFileSync('../cert/cert.pem'),
-        requestCert:true,  //请求客户端证书
-        // rejectUnauthorized:false 
-        // rejectUnauthorized:false
-    };
-    var req = https.get(options, function(res){ 
-      // res.pipe(process.stdout); 
-      // console.log(res.pipe(process.stdout));
-        var data = '';
-        res.on('data',function(buff){
-            data += buff;//查询结果（JSON格式）
-            console.log(data)
-        });
-        res.on('end',function(){
-            console.log(data)
-        }); 
-        console.log(res);
-    });
+router.get("/", function(req,res,next){
 
-    req.on('error', function(err){
-      console.error(err.code);
-    });
-   
-    
-  // 用 superagent 去抓取 https://cnodejs.org/ 的内容
+let lists = []
 
-  // superagent.get('https://www.baidu.com')
-  //   .end(function (err, sres) {
-  //     // 常规的错误处理
-  //     if (err) {
-  //       return next(err);
-  //     }
-  //     // sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load 之后
-  //     // 就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
-  //     // 剩下就都是 jquery 的内容了
-  //     console.log(sres);
-  //     console.log(sres.text);
-  //     var $ = cheerio.load(sres.text);
-  //     var items = [];
-  //     // $('#topic_list .topic_title').each(function (idx, element) {
-  //     //   var $element = $(element);
-  //     //   items.push({
-  //     //     title: $element.attr('title'),
-  //     //     href: $element.attr('href')
-  //     //   });
-  //     // });
-
-  //     res.send(items);
-  //   });
-// });
-// var yz_temp = '',yw_temp = '';//保存余票状态
-// router.get("/", function(req,res,next){
-// async function queryTickets(config){
-//     const url = 'https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date='+config.time+'&leftTicketDTO.from_station='+config.from_station+'&leftTicketDTO.to_station='+config.end_station+'&purpose_codes=ADULT';
-//     const result = await request(url, {
-//       headers: {
-//         'verify': 'false'
-//       }
-//     });
-//     console.log(result.body)
-//     // var options = { 
-//     //     hostname: 'kyfw.12306.cn',//12306
-//     //     path: '/otn/leftTicket/queryA?leftTicketDTO.train_date='+config.time+'&leftTicketDTO.from_station='+config.from_station+'&leftTicketDTO.to_station='+config.end_station+'&purpose_codes=ADULT',
-//     //     ca:[ca],//证书
-//     //     rejectUnauthorized:false
-//     // };
-//     // var req = https.get(options, function(res){ 
-//     //   res.pipe(process.stdout); 
-//     //   console.log(res.pipe(process.stdout));
-//     // });
-
-//     // req.on('error', function(err){
-//     //   console.error(err.code);
-//     // });
-//     // var req = https.get(options, function(res){ 
-//     // var data = '';
-//     // res.on('data',function(buff){
-//     //     data += buff;//查询结果（JSON格式）
-//     //     console.log(data)
-//     // }); 
-//     // res.on('end',function(){
-//     //     console.log('res',data);
-//     //     // var jsonData = JSON.parse(data).data;
-//     //     // console.log('res',jsonData);
-//     //     // for(var i=0;i<jsonData.length;i++){
-//     //     //     var cur = jsonData[i];
-//     //     // }
-//     //     // fs.writeFile('./train.json',data);
-//     //     })  
-//     // });
-//     // req.on('error', function(err){
-//     //     console.error(err.code);
-//     // });
-//     }
-//     queryTickets(config)
-// });
+superagent.get(url) //这里的URL也可以是绝对路径
+        .end(function(req,res){
+            const r = JSON.parse(res.text)
+            const flag = r.data.flag
+            const map = r.data.map
+            const result = r.data.result
+            result.forEach((item,index)=>{
+              let list = {}
+              list.train_id = item.split('|')[2].toString()
+              list.train_no = item.split('|')[3].toString()
+              list.from_station_name = item.split('|')[4].toString()
+              list.to_station_name = item.split('|')[5].toString()
+              list.start_time = item.split('|')[8].toString()
+              list.arrive_time = item.split('|')[9].toString()
+              list.duration = item.split('|')[10].toString()
+              list.if_can_by = item.split('|')[11].toString()
+              list.leave_time = item.split('|')[13].toString()
+              list.seat_type = item.split('|')[15].toString()
+              lists.push(list)
+            })
+            res.json({
+                status:true,
+                msg:'查询成功',
+                data:{'result': lists}
+            });
+        })
+  
+});
 
 module.exports = router;
