1a750a4b6458bd5295a6ba03ac0c6ad87723a085
ltt <leitingting08@gmail.com>
2018-10-15 20:02:15 +0800

    stations

--- server/models/stations.js
+++ server/models/stations.js
@@ -7,7 +7,6 @@
 var superagent = require('superagent')
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-// var db = new     mongoose.createConnection('mongodb://127.0.0.1:27017/train');
 
 var stationSchema = new Schema({ //新建数据表
     "sta_name":String, // 车站名称
@@ -20,8 +19,8 @@ const url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?st
 let stalists = []
 superagent.get(url)
   .end(function(response,result){
-	  const r = result.text.split('=')[1]
-	  var arr = r.split('@')
+	  let r = result.text.split('=')[1]
+	  let arr = r.split('@')
 	  arr.forEach((i,index)=>{
 	    let list = {}
 	    list.sta_name = i.split('|')[1];
@@ -29,19 +28,18 @@ superagent.get(url)
 	    list.sta_code = i.split('|')[2];
 	    stalists.push(list);
 	  })
-   
+
+	  monModel.find({},function(err,docs){
+	  	console.log(docs.length)
+	  	if(!docs.length){ // 如果数据库里有数据就不插入，否则就插入数据
+	  	  // 把车站信息写入数据库
+	      monModel.insertMany(stalists, function(err, docs){
+		        if(err) console.log(err);
+		        console.log('车站信息写入成功：' + docs);
+		  });
+	  	}
+	  })
 })
 
-// 把车站信息写入数据库
-// var monInsert = new monModel();
-var monInsert = monModel.insertMany(stalists);
 
-// monInsert.save(function(err){
-//    if(err){
-//    console.log(err);
-//    }else{
-//    console.log('车站信息写入成功');
-//    }
-// });
 module.exports = monModel;
-// module.exports = mongoose.model('Station',stationSchema);
